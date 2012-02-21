43f482c4014e49de13334035c08d55156f629d51
Satoru Ohno <mochablendy@gmail.com>
2012-02-21 17:32:15 +0900

    add and modify some files to implement lap time importer.

---
+++ public/js/mose.lapbookmarklet.js
@@ -0,0 +1,14 @@
+javascript:( function(){
+	var j = document.createElement('script');
+	j.charset='UTF-8';
+	j.src='http://localhost:3000/js/jquery-1.7.1.min.js';
+	document.body.appendChild(j);
+	var s = document.createElement('script');
+	s.charset='UTF-8';
+	s.src='http://localhost:3000/js/mose.laptimeimporter.js';
+	document.body.appendChild(s);
+	var f = document.createElement('p');
+	f.id = 'setup';
+	f.innerHTML = 'foo.htm';
+	document.body.appendChild(f);
+})();

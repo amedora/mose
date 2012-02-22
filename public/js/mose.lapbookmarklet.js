3c40b0fdbc41b7e51dcf2c3a577d82aa9a8c5611
Satoru Ohno <mochablendy@gmail.com>
2012-02-22 16:43:57 +0900

    some codes added to implement lap time manager.

--- public/js/mose.lapbookmarklet.js
+++ public/js/mose.lapbookmarklet.js
@@ -1,11 +1,11 @@
 javascript:( function(){
 	var j = document.createElement('script');
 	j.charset='UTF-8';
-	j.src='http://localhost:3000/js/jquery-1.7.1.min.js';
+	j.src="http://localhost:3000/js/jquery-1.7.1.min.js";
 	document.body.appendChild(j);
 	var s = document.createElement('script');
 	s.charset='UTF-8';
-	s.src='http://localhost:3000/js/mose.laptimeimporter.js';
+	s.src='http://localhost:3000/laptimeimporter.js';
 	document.body.appendChild(s);
 	var f = document.createElement('p');
 	f.id = 'setup';

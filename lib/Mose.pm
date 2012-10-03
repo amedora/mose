c072593e168ecf8867534a9ca7bea92780fb184b
Satoru Ohno <mochablendy@gmail.com>
2012-10-03 11:25:53 +0900

    Fixed Analysis views. (still need fix to Analysis actions)

--- lib/Mose.pm
+++ lib/Mose.pm
@@ -1,22 +1,25 @@
 # vim:set sw=4 ts=4 ft=perl:
 package Mose;
 use Mojo::Base 'Mojolicious';
+use File::Basename 'dirname';
+use File::Spec::Functions 'catdir';
 
 sub startup {
-	my $self = shift;
-
-	$self->secret('mose');
-	plugin Config => { file => 'mose.conf' };
-
-	my $r = $self->routes;
-	# TODO: define routes here
-}
-
-get '/' => sub {
     my $self = shift;
 
-    #$self->render('index');
-    $self->redirect_to('analysis/latemodel');
-};
+    $self->secret('mose');
+
+    $self->plugin( 'Config', { file => 'mose.conf' } );
+
+    $self->home->parse( catdir( dirname(__FILE__), 'Mose' ) );
+    $self->static->paths->[0]   = $self->home->rel_dir('public');
+    $self->renderer->paths->[0] = $self->home->rel_dir('templates');
+
+    my $r = $self->routes;
+    $r->get('/')->to('root#index');
+
+    # Analysis
+    $r->get('/analysis/:car')->to('analysis#index');
+}
 
 1;
--- lib/Mose/Analysis.pm
+++ lib/Mose/Analysis.pm
@@ -1,11 +1,13 @@
 # vim:set sw=4 ts=4 ft=perl:
 package Mose::Analysis;
 use Mojo::Base 'Mojolicious::Controller';
+use File::Basename;
+use File::Find::Rule;
 
-get '/analysis/:car' => sub {
+sub index {
     my $self      = shift;
     my $car       = $self->param('car');
-    my $searchdir = $self->stash('config')->{setupdir} . '/' . $car;
+    my $searchdir = $self->stash->{config}->{setupdir} . '/' . $car;
     my @files     = File::Find::Rule->file()->name('*.htm')->in($searchdir);
     my @setups    = map {
         my $fullpath = $_;
@@ -17,8 +19,8 @@ get '/analysis/:car' => sub {
             fullpath => $fullpath
         };
     } @files;
-
-    $self->render( 'analysis', setups => \@setups, car => $car );
-};
+	$self->stash->{setups} = \@setups;
+	$self->stash->{car} = $car;
+}
 
 1;

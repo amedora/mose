0a4135260b6c57946bd0db2aa13007f65a016b64
mochablendy <mochablendy@gmail.com>
2012-10-25 16:47:25 +0900

    setup files listing is now helper-ed.

--- lib/Mose/Analysis.pm
+++ lib/Mose/Analysis.pm
@@ -1,27 +1,13 @@
 # vim:set sw=4 ts=4 ft=perl:
 package Mose::Analysis;
 use Mojo::Base 'Mojolicious::Controller';
-use File::Basename;
-use File::Find::Rule;
 
 use IRacing::Setup::Parser;
 
 sub home {
     my $self      = shift;
     my $car       = $self->param('car');
-    my $searchdir = $self->stash->{config}->{setupdir} . '/' . $car;
-    my @files     = File::Find::Rule->file()->name('*.htm')->in($searchdir);
-    my @setups    = map {
-        my $fullpath = $_;
-        my ( $filename, $dir ) = fileparse($fullpath);
-        $dir =~ s/.*?$car//;
-        +{
-            filename => $filename,
-            dir      => $dir,
-            fullpath => $fullpath
-        };
-    } @files;
-	$self->stash->{setups} = \@setups;
+	$self->stash->{setups} = $self->app->setups($car);
 	$self->stash->{car} = $car;
 }
 

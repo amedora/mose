e8ad43e784c277481e0d9ac46ab62d76e5c35071
mochablendy <mochablendy@gmail.com>
2013-05-08 16:21:11 +0900

    Ford Fusion Gen6 is supported. (not completed yet)

--- lib/IRacing/Setup/Parser.pm
+++ lib/IRacing/Setup/Parser.pm
@@ -169,100 +169,124 @@ __DATA__
 TIRE:
   LEFT FRONT:
     Cold pressure:
+      stockcars fordfusion: '/html/body/u[1]'
       latemodel: '/html/body/u[1]'
       trucks silverado: '/html/body/u[1]'
       stockcars2 chevy cot: '/html/body/u[1]'
     Last hot pressure:
+      stockcars fordfusion: '/html/body/u[2]'
       latemodel: '/html/body/u[2]'
       trucks silverado: '/html/body/u[2]'
       stockcars2 chevy cot: '/html/body/u[2]'
     Last temps O:
+      stockcars fordfusion: '/html/body/u[3]'
       latemodel: '/html/body/u[3]'
       trucks silverado: '/html/body/u[3]'
       stockcars2 chevy cot: '/html/body/u[3]'
     Last temps M:
+      stockcars fordfusion: '/html/body/u[4]'
       latemodel: '/html/body/u[4]'
       trucks silverado: '/html/body/u[4]'
       stockcars2 chevy cot: '/html/body/u[4]'
     Last temps I:
+      stockcars fordfusion: '/html/body/u[5]'
       latemodel: '/html/body/u[5]'
       trucks silverado: '/html/body/u[5]'
       stockcars2 chevy cot: '/html/body/u[5]'
     Tread remaining O:
+      stockcars fordfusion: '/html/body/u[6]'
       latemodel: '/html/body/u[6]'
       trucks silverado: '/html/body/u[6]'
       stockcars2 chevy cot: '/html/body/u[6]'
     Tread remaining M:
+      stockcars fordfusion: '/html/body/u[7]'
       latemodel: '/html/body/u[7]'
       trucks silverado: '/html/body/u[7]'
       stockcars2 chevy cot: '/html/body/u[7]'
     Tread remaining I:
+      stockcars fordfusion: '/html/body/u[8]'
       latemodel: '/html/body/u[8]'
       trucks silverado: '/html/body/u[8]'
       stockcars2 chevy cot: '/html/body/u[8]'
   LEFT REAR:
     Cold pressure:
+      stockcars fordfusion: '/html/body/u[9]'
       latemodel: '/html/body/u[9]'
       trucks silverado: '/html/body/u[9]'
       stockcars2 chevy cot: '/html/body/u[9]'
     Last hot pressure:
+      stockcars fordfusion: '/html/body/u[10]'
       latemodel: '/html/body/u[10]'
       trucks silverado: '/html/body/u[10]'
       stockcars2 chevy cot: '/html/body/u[10]'
     Last temps O:
+      stockcars fordfusion: '/html/body/u[11]'
       latemodel: '/html/body/u[11]'
       trucks silverado: '/html/body/u[11]'
       stockcars2 chevy cot: '/html/body/u[11]'
     Last temps M:
+      stockcars fordfusion: '/html/body/u[12]'
       latemodel: '/html/body/u[12]'
       trucks silverado: '/html/body/u[12]'
       stockcars2 chevy cot: '/html/body/u[12]'
     Last temps I:
+      stockcars fordfusion: '/html/body/u[13]'
       latemodel: '/html/body/u[13]'
       trucks silverado: '/html/body/u[13]'
       stockcars2 chevy cot: '/html/body/u[13]'
     Tread remaining O:
+      stockcars fordfusion: '/html/body/u[14]'
       latemodel: '/html/body/u[14]'
       trucks silverado: '/html/body/u[14]'
       stockcars2 chevy cot: '/html/body/u[14]'
     Tread remaining M:
+      stockcars fordfusion: '/html/body/u[15]'
       latemodel: '/html/body/u[15]'
       trucks silverado: '/html/body/u[15]'
       stockcars2 chevy cot: '/html/body/u[15]'
     Tread remaining I:
+      stockcars fordfusion: '/html/body/u[16]'
       latemodel: '/html/body/u[16]'
       trucks silverado: '/html/body/u[16]'
       stockcars2 chevy cot: '/html/body/u[16]'
   RIGHT FRONT:
     Cold pressure:
+      stockcars fordfusion: '/html/body/u[17]'
       latemodel: '/html/body/u[17]'
       trucks silverado: '/html/body/u[17]'
       stockcars2 chevy cot: '/html/body/u[17]'
     Last hot pressure:
+      stockcars fordfusion: '/html/body/u[18]'
       latemodel: '/html/body/u[18]'
       trucks silverado: '/html/body/u[18]'
       stockcars18 chevy cot: '/html/body/u[18]'
     Last temps O:
+      stockcars fordfusion: '/html/body/u[19]'
       latemodel: '/html/body/u[19]'
       trucks silverado: '/html/body/u[19]'
       stockcars2 chevy cot: '/html/body/u[19]'
     Last temps M:
+      stockcars fordfusion: '/html/body/u[20]'
       latemodel: '/html/body/u[20]'
       trucks silverado: '/html/body/u[20]'
       stockcars2 chevy cot: '/html/body/u[20]'
     Last temps I:
+      stockcars fordfusion: '/html/body/u[21]'
       latemodel: '/html/body/u[21]'
       trucks silverado: '/html/body/u[21]'
       stockcars2 chevy cot: '/html/body/u[21]'
     Tread remaining O:
+      stockcars fordfusion: '/html/body/u[22]'
       latemodel: '/html/body/u[22]'
       trucks silverado: '/html/body/u[22]'
       stockcars2 chevy cot: '/html/body/u[22]'
     Tread remaining M:
+      stockcars fordfusion: '/html/body/u[23]'
       latemodel: '/html/body/u[23]'
       trucks silverado: '/html/body/u[23]'
       stockcars2 chevy cot: '/html/body/u[23]'
     Tread remaining I:
+      stockcars fordfusion: '/html/body/u[24]'
       latemodel: '/html/body/u[24]'
       trucks silverado: '/html/body/u[24]'
       stockcars2 chevy cot: '/html/body/u[24]'
@@ -270,34 +294,42 @@ TIRE:
       latemodel: '/html/body/u[25]'
   RIGHT REAR:
     Cold pressure:
+      stockcars fordfusion: '/html/body/u[25]'
       latemodel: '/html/body/u[26]'
       trucks silverado: '/html/body/u[25]'
       stockcars2 chevy cot: '/html/body/u[25]'
     Last hot pressure:
+      stockcars fordfusion: '/html/body/u[26]'
       latemodel: '/html/body/u[27]'
       trucks silverado: '/html/body/u[26]'
       stockcars2 chevy cot: '/html/body/u[26]'
     Last temps O:
+      stockcars fordfusion: '/html/body/u[27]'
       latemodel: '/html/body/u[28]'
       trucks silverado: '/html/body/u[27]'
       stockcars2 chevy cot: '/html/body/u[27]'
     Last temps M:
+      stockcars fordfusion: '/html/body/u[28]'
       latemodel: '/html/body/u[29]'
       trucks silverado: '/html/body/u[28]'
       stockcars2 chevy cot: '/html/body/u[28]'
     Last temps I:
+      stockcars fordfusion: '/html/body/u[29]'
       latemodel: '/html/body/u[30]'
       trucks silverado: '/html/body/u[29]'
       stockcars2 chevy cot: '/html/body/u[29]'
     Tread remaining O:
+      stockcars fordfusion: '/html/body/u[30]'
       latemodel: '/html/body/u[31]'
       trucks silverado: '/html/body/u[30]'
       stockcars2 chevy cot: '/html/body/u[30]'
     Tread remaining M:
+      stockcars fordfusion: '/html/body/u[31]'
       latemodel: '/html/body/u[32]'
       trucks silverado: '/html/body/u[31]'
       stockcars2 chevy cot: '/html/body/u[31]'
     Tread remaining I:
+      stockcars fordfusion: '/html/body/u[32]'
       latemodel: '/html/body/u[33]'
       trucks silverado: '/html/body/u[32]'
       stockcars2 chevy cot: '/html/body/u[32]'
@@ -306,20 +338,25 @@ TIRE:
 CHASSIS:
   FRONT:
     Ballast forward:
+      stockcars fordfusion: '/html/body/u[33]'
       latemodel: '/html/body/u[35]'
       trucks silverado: '/html/body/u[33]'
       stockcars2 chevy cot: '/html/body/u[33]'
     Nose weight:
+      stockcars fordfusion: '/html/body/u[34]'
       latemodel: '/html/body/u[36]'
       trucks silverado: '/html/body/u[34]'
       stockcars2 chevy cot: '/html/body/u[34]'
     Cross weight:
+      stockcars fordfusion: '/html/body/u[35]'
       latemodel: '/html/body/u[37]'
     Toe-in:
       latemodel: '/html/body/u[38]'
     Steering ratio:
+      stockcars fordfusion: '/html/body/u[36]'
       latemodel: '/html/body/u[39]'
     Front brake bias:
+      stockcars fordfusion: '/html/body/u[37]'
       latemodel: '/html/body/u[40]'
     Sway bar size:
       latemodel: '/html/body/u[41]'
@@ -330,6 +367,7 @@ CHASSIS:
     Attach left side:
       latemodel: '/html/body/u[44]'
     Tape Configuration:
+      stockcars fordfusion: '/html/body/u[38]'
       latemodel: '/html/body/u[45]'
   LEFT FRONT:
     Corner weight:

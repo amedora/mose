4046aca1e184622c65f89e1c9bb98ce49df28eb0
mochablendy <mochablendy@gmail.com>
2015-01-19 10:40:59 +0900

    clean up unnecessary code and file

--- public/js/mose.js
+++ public/js/mose.js
@@ -102,31 +102,4 @@ var mose = mose || {};
         });
     }
 
-    function get_data() {
-        tableData.fnDestroy();
-        $.ajax({
-            type: 'GET',
-            url: '/analysis/datatable?' + $('#form_fileselect').serialize(),
-            success: function(html) {
-                $('#tabledata').replaceWith(html);
-                tableData = $('#tabledata').dataTable({
-                    "iDisplayLength": 150,
-                    "aaSorting": [],
-                    "sDom": "<'row'<'col-md-4'l><'col-md-4'T><'col-md-4'f>r><'row'<'col-md-12't>><'row'<'col-md-5'i><'col-md-7'p>>",
-                    "oTableTools": {
-                        "sSwfPath": '/swf/copy_csv_xls_pdf.swf',
-                        "aButtons": ['copy', 'csv', 'pdf', 'print']
-                    },
-                    /*
-                "sDom" : "<'row'<'col-md-6'l><'col-md-6'f>r>t<'row'<'col-md-6'i><'col-md-6'p>>",
-				*/
-                    "sPaginationType": "bootstrap",
-                    "oLanguage": {
-                        "sLengthMenu": "_MENU_ components per page"
-                    }
-                });
-            }
-        });
-    }
-
 })(mose);
